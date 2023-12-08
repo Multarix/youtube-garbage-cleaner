cce62916fef651e173ae0251de81313c5b53be3d
Darien <darien2105@gmail.com>
2023-12-08 18:37:24 +1000

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,32 +1,45 @@
 // ==UserScript==
 // @name         Hide Youtube Garbage
-// @version      1.1
-// @description  Removes shorts or other garbage from youtube Tabs
+// @version      1.2
+// @description  Removes shorts or other absolute garbage that youtube thinks we care about.
 // @author       Multarix
+// @supportURL   https://github.com/Multarix/youtube-garbage-cleaner/issues
+// @homepageURL  https://github.com/Multarix/youtube-garbage-cleaner/
 // @match        http://*.youtube.com/*
 // @match        https://*.youtube.com/*
+// @match        *youtube*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
-// @run-at       document-idle
+// @downloadURL  https://github.com/Multarix/youtube-garbage-cleaner/raw/master/script.user.js
+// @updateURL     https://github.com/Multarix/youtube-garbage-cleaner/raw/master/script.user.js
 // ==/UserScript==
 
 let garbageCleanerinterval;
+let count = 0;
 
-const garbagePile = ["YouTube Premium", "Shorts", "Trending", "Music", "News", "Sport", "Learning", "Fashion & beauty", "Youtube Kids"];
+const garbagePile = ["YouTube Premium", "Shorts", "Trending", "Music", "News", "Sport", "Learning", "Fashion & beauty", "Youtube Kids", "Gaming"];
 
-const garbageCleaner = () => {
+const garbageCleaner = async () => {
 	console.log("Attempting disposal of garbage...");
-	if(document.querySelectorAll(`[title='${garbagePile[2]}']`).length > 0){
+    count++;
+
+    for(const garbage of garbagePile){
+		document.querySelectorAll(`[title='${garbage}']`).forEach(x => x.remove());
+		document.querySelectorAll(`[href*='premium']`).forEach(x => x.remove());
+		document.querySelectorAll(`[href*='youtubekids']`).forEach(x => x.remove());
+	}
+
+
+	if(document.querySelectorAll(`[title='${garbagePile[1]}']`).length > 0){
 		clearInterval(garbageCleanerinterval);
 		console.log("Garbage disposed of successfully!");
 	}
 
 
-	for(const garbage of garbagePile){
-		document.querySelectorAll(`[title='${garbage}']`).forEach(x => x.remove());
-		document.querySelectorAll(`[href*='premium']`).forEach(x => x.remove());
-		document.querySelectorAll(`[href*='youtubekids']`).forEach(x => x.remove());
-	}
+    if(count >= 20){
+		clearInterval(garbageCleanerinterval);
+        console.error("GARBAGE WAS UNABLE TO BE DISPOSED OF");
+    }
 };
 
 
